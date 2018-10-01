8b75e569d2a0115b05161095d054644a6e4bea60
buddythumbs <moleary1987@gmail.com>
2018-10-01 11:29:04 +0100

    adding in clog to print out environment variable for config

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+# Print out environ
+print(os.environ['APP_SETTINGS'])
+
+
 @app.route('/')
 def hello():
     return "Hello World!"
